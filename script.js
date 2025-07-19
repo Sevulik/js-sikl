4f401be358111290a598ba209a4e040e9c3a7553
Sevulik <srcseval@gmail.com>
2025-07-19 18:19:06 +0500

    Normativ js 2

---
+++ script.js
@@ -0,0 +1,52 @@
+// js 2.1
+
+// let count = 0
+// while (count < 20){
+//     count += 1
+//     if (count % 2 === 0){
+//         console.log(count);      
+//     } 
+// }
+
+// js 2.2
+
+// let input = prompt("Enter numbers with spaces: ");
+// let numbers = input.split(" ").map(Number); 
+// let sum = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+// }
+
+// console.log("Sum:", sum);
+
+
+// js 2.3
+
+// let arr = [-2, 4, -4, -5, 8, -9, 3, 5, 12];
+// let sum = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0 && arr[i] % 2 === 0) {
+//         sum += arr[i];
+//     }
+// }
+// console.log(sum);
+
+// js 2.4
+
+
+// let count = 0
+// while (count < 100){
+//     count += 1
+//     if (count % 3 === 0 && count % 5 === 0){
+//         console.log(count);      
+//     }
+// }
+
+// js 2.5
+
+// let arr = [2, -8, 2, 1, 5, 4, -15, 9, 13]
+// let res = Math.max(...arr)
+// let res2 = Math.min(...arr)
+// console.log(res, res2);
